c4e92f8bcbdd6caf6b33d7cf6a0d76b2be803e80
Duksara <tadicz84@gmail.com>
2025-08-21 11:44:03 +0200

    chore: Add mui css for upload subscriber modal

--- src/components/SubscriberBreakdown/UploadSubscriberModal/UploadSubscriberModal.tsx
+++ src/components/SubscriberBreakdown/UploadSubscriberModal/UploadSubscriberModal.tsx
@@ -10,6 +10,7 @@ import {
   Box,
   Divider,
 } from "@mui/material";
+import styles from "./UploadSubscriberModal.module.css";
 
 interface UploadSubscriberModalProps {
   open: boolean;
@@ -43,171 +44,56 @@ const UploadSubscriberModal: React.FC<UploadSubscriberModalProps> = ({
   };
 
   return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      sx={{
-        "& .MuiDialog-paper": {
-          width: "845px",
-          height: "498px",
-          maxWidth: "none",
-          maxHeight: "none",
-          borderRadius: "18px",
-          padding: "30px 46px 24px 26px",
-        },
-      }}
-    >
-      <DialogTitle
-        sx={{
-          fontWeight: 700,
-          fontSize: "25px",
-          color: "#050e21",
-          fontFamily:
-            '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-        }}
-      >
+    <Dialog open={open} onClose={onClose} className={styles.customDialog}>
+      <DialogTitle className={styles.dialogTitle}>
         Upload Subscriber List
       </DialogTitle>
 
       <DialogContent>
-        <Typography
-          sx={{
-            fontWeight: "400",
-            color: "#050e21",
-            fontSize: "20px",
-            fontFamily:
-              '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-            marginTop: "-4px",
-          }}
-        >
+        <Typography className={styles.dialogSubtitle}>
           Here you can see an example of how the csv file must be formatted.
         </Typography>
 
-        <Typography
-          sx={{
-            fontFamily:
-              '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-            fontWeight: "400",
-            fontSize: "20px",
-            color: "#050e21",
-            textDecoration: "underline",
-            marginTop: "6px",
-            "&:hover": {
-              cursor: "pointer",
-            },
-          }}
-        >
-          Example
-        </Typography>
+        <Typography className={styles.textLink}>Example</Typography>
 
-        <Box sx={{ marginTop: "10px" }}>
-          <Typography
-            sx={{
-              fontSize: "18px",
-              fontWeight: "700",
-              color: "#050E21",
-              fontFamily:
-                '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-            }}
-          >
-            List name
-          </Typography>
+        <Box className={styles.box}>
+          <Typography className={styles.inputLabel}>List name</Typography>
           <TextField
             value={listName}
             onChange={(e) => setListName(e.target.value)}
-            sx={{
-              marginBottom: "16px",
-              "& .MuiOutlinedInput-root": {
-                "& fieldset": {
-                  borderColor: "#050E21",
-                },
-                width: "454px",
-                height: "44px",
-              },
-            }}
+            className={styles.input}
           />
         </Box>
 
         <Box>
-          <Typography
-            variant="body2"
-            sx={{
-              fontSize: "15px",
-              fontWeight: "300",
-              color: "#050E21",
-              fontFamily:
-                '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-            }}
-          >
+          <Typography variant="body2" className={styles.instructionText}>
             Upload Content: You can upload csv file
           </Typography>
 
           <input
             accept=".csv"
-            style={{ display: "none" }}
+            className={styles.hiddenFileInput}
             id="file-upload"
             type="file"
             onChange={handleFileChange}
           />
           <label htmlFor="file-upload">
-            <Button
-              sx={{
-                color: "#FFFFFF",
-                width: "226px",
-                height: "47px",
-                borderRadius: "50px",
-                background: "#050E21",
-                fontWeight: 700,
-                fontSize: "15px",
-                fontFamily:
-                  '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-                textDecoration: "uppercase",
-                marginTop: "18px",
-              }}
-            >
-              Upload file
-            </Button>
+            <Button className={styles.uploadButton}>Upload file</Button>
           </label>
         </Box>
       </DialogContent>
 
-      <Divider sx={{ margin: "0px 10px 20px 26px" }} />
+      <Divider className={styles.divider} />
 
       <DialogActions>
         <Button
           onClick={handleCancel}
           variant="outlined"
-          sx={{
-            color: "#050E21",
-            width: "154px",
-            height: "47px",
-            borderRadius: "50px",
-            borderColor: "#050E21",
-            fontWeight: 700,
-            fontSize: "15px",
-            fontFamily:
-              '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-            "&:hover": {
-              borderColor: "#050E21",
-            },
-          }}
+          className={styles.cancelButton}
         >
           Cancel
         </Button>
-        <Button
-          onClick={handleSubmit}
-          sx={{
-            color: "#FFFFFF",
-            width: "185px",
-            height: "47px",
-            borderRadius: "50px",
-            backgroundColor: "#050E21",
-            fontWeight: 700,
-            fontSize: "15px",
-            fontFamily:
-              '"Nunito Sans", system-ui, Avenir, Helvetica, Arial, sans-serif',
-          }}
-        >
+        <Button onClick={handleSubmit} className={styles.submitButton}>
           Generate
         </Button>
       </DialogActions>
