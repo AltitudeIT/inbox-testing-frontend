22e00ac5648836b691be93b49c38f51cc326c5b9
Duksara <tadicz84@gmail.com>
2025-08-29 12:48:11 +0200

    chore: Add account header

--- src/components/Layout/ProfileLayout/ProfileLayout.tsx
+++ src/components/Layout/ProfileLayout/ProfileLayout.tsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router";
+import { Box, Typography } from "@mui/material";
 import styles from "./ProfileLayout.module.css";
 import ProfileSidebar from "../../ProfileSidebar/ProfileSidebar";
+import PageTitle from "../../PageTitle/PageTitle";
 
 const ProfileLayout = () => {
   return (
-    <div className={styles.rootDiv}>
-      <ProfileSidebar />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
-    </div>
+    <Box>
+      <Box className={styles.titleBox}>
+        <PageTitle title="Account" />
+      </Box>
+      <Box className={styles.rootDiv}>
+        <ProfileSidebar />
+        <main className={styles.main}>
+          <Outlet />
+        </main>
+      </Box>
+    </Box>
   );
 };
 
